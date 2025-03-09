24f36626862eda798909e6d5407717f28de29f6f
Gasaii <mrprofer786@gmail.com>
2025-03-09 12:16:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    alert("Привет! Сайт загружен.");
+    let tg = window.Telegram.WebApp; // Подключаем WebApp API
+
+    tg.expand(); // Разворачиваем WebApp на весь экран
+
+    document.getElementById("btn").addEventListener("click", function () {
+        let data = { action: "get_balance" }; // Данные для бота
+
+        tg.sendData(JSON.stringify(data)); // Отправляем JSON-объект в бота
+        tg.close(); // Закрываем WebApp после отправки
+    });
 });
